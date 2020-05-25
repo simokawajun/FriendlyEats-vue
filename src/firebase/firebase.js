9660922af02f9bfeca3658b6f9f4ec1b109077d6
simokawajun <43632159+simokawajun@users.noreply.github.com>
2020-05-26 08:48:49 +0900

    Update firebase.js
    const firebaseConfig　変更

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,11 @@
 const firebaseConfig = {
+  apiKey: "AIzaSyChStvxTWlasGdQCXnEcconKfrE7aAsGms",
+  authDomain: "friendlyeatsdd.firebaseapp.com",
+  databaseURL: "https://friendlyeatsdd.firebaseio.com",
+  projectId: "friendlyeatsdd",
+  storageBucket: "friendlyeatsdd.appspot.com",
+  messagingSenderId: "1000357390322",
+  appId: "1:1000357390322:web:2ecdd60ae54547ff7b2d6b",
+  measurementId: "G-0J7F589CS7"
 };
-
 export default firebaseConfig
